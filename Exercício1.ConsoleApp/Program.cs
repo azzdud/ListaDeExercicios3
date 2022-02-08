cc05a02b89be9283b97c03b92a57ad2f372d23a2
Luís Eduardo Bernardino Luz <dudubluz@gmail.com>
2022-02-08 14:04:55 -0300

    Add project files.

---
+++ Exercício1.ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Exercício 1
+ * Desenvolver um algoritmo que leia a altura de 15 pessoas. Este
+programa deverá calcular e mostrar:
+a. A menor altura do grupo;
+b. A maior altura do grupo;*/
+
+using System;
+
+namespace Exercício1.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal altura;
+
+            Console.WriteLine("Digite a altura 1: ");
+        }
+    }
+}
