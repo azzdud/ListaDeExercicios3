82794b8fd7c3df651a517786d64a6d9eaa8d7593
Luís Eduardo Bernardino Luz <dudubluz@gmail.com>
2022-02-09 16:25:01 -0300

    Exercício 5

---
+++ Exercício5.ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Exercício 5
+ * Escreva um algoritmo que leia um valor inicial A e imprima a sequência de
+valores do cálculo de A! e o seu resultado.
+Ex: 5! = 5 X 4 X 3 X 2 X 1 = 120
+• Pesquise sobre “fatorial”*/
+
+using System;
+
+namespace Exercício5.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número para saber seu fatorial: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = 1;
+
+            for (int i = num; i > 0; i--)
+            {
+                if (i != 1)
+                    Console.Write(i + " x ");
+                else
+                    Console.Write(i);
+
+                resultado = resultado * i;
+            }            
+            
+            Console.Write(" = ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(resultado);
+
+            Console.ReadLine();
+        }
+    }
+}
